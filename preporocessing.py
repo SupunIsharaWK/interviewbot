170a931d3ca57be4343aa498479dbd7feb88b79f
Supun Ishara Weerasekara <SupunIsharaWK@users.noreply.github.com>
2018-06-19 22:46:32 +0530

    Update preporocessing.py

--- preporocessing.py
+++ preporocessing.py
@@ -1,7 +1,7 @@
 # encoding=utf8
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf8')
 import pickle
 import spacy
 import numpy as np
